ed71a0d0f9c2686fb235782d0a066540ea7bbcb5
FrancescoDiT <francesco2003.fdt@gmail.com>
2024-04-09 19:51:12 +0200

    First commit

---
+++ src/main/java/org/elis/rpexternalsbackend/RpExternalsBackendApplication.java
@@ -0,0 +1,13 @@
+package org.elis.rpexternalsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpExternalsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RpExternalsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/elis/rpexternalsbackend/RpExternalsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.elis.rpexternalsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpExternalsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
